b291b98176bd637bbef7c412fad122176504515b
Ansh-2303 <anshpatel184@gmail.com>
2024-06-08 12:18:02 +0530

    main files

---
+++ day 4 first python program.py
@@ -0,0 +1,8 @@
+# print is a built in function used to display anything on screen 
+print("hello world")
+print(11)
+print("how","are","you")
+# in above print statement, all objects will be printed and they will seperated by a space by defualt. 
+print(50+2)
+
+
---
+++ day 5 comments and escape sequences.py
@@ -0,0 +1,11 @@
+# comments are non executable lines which are used to describe the code and tell the purpose of the code.
+# single line comments: #
+# multiline comments: ''' comments '''
+ 
+# escape sequence is used to insert a character that cannot be directly be used in a string
+print("work hard\npush yourself")
+
+# sep and end
+print("hey",12,sep="@", end="%")
+print("python")
+# by defualt end value is \n and sep value is a space
