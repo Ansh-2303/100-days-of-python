bb06f2bf21cf6d62d2e73495dbf34215182036e5
Ansh-2303 <anshpatel184@gmail.com>
2024-06-08 20:59:55 +0530

    main files

---
+++ day 6 variables and datatypes.py
@@ -0,0 +1,25 @@
+# variables are like containers used to store a value.
+# datatype is type of data that a variable can store.
+a=1
+print(a)
+b="Ansh"
+c=True
+d=None
+
+# print(a+b) error
+
+print(type(d))
+print(type(c))
+
+lst=[1,2,[3,4]]
+print(lst)
+tup=(1,2,3,4)
+dict={1:"ansh",b:"student"}
+print(dict[1])
+
+# everything in python is an object. for eg, above dictionary is an object.
+
+
+
+
+
